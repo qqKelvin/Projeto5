f08e5033f628440972b3c469966035ded8ba226c
Kelvin Rodrigues <kelvinmice77@gmail.com>
2025-02-18 00:10:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv("C:\\Users\\kelvi\\Projeto5\\vehicles.csv")
+
+st.title("Análise de Dados de Veículos")
+
+if st.button("Mostrar Dados"):
+    st.dataframe(car_data)
+
+fig = px.histogram(car_data, x="odometer", color="condition", title="Distribuição do Odômetro")
+st.plotly_chart(fig)
+
+fig = px.scatter(car_data, x="odometer", y="price", title="Odômetro x Preço")
+st.plotly_chart(fig)
