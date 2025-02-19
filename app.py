a6a233cf9646f3a2d6b8be1c10ae04074eae458c
GitKelvin <kelvinmice77@gmail.com>
2025-02-19 19:20:54 -0300

    alterações

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv("C:\\Users\\kelvi\\Projeto5\\vehicles.csv")
+car_data = pd.read_csv("vehicles.csv")
+
 st.title("Análise de Dados de Veículos")
 
 if st.button("Mostrar Dados"):
